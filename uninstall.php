bcc9e8c9e5a889b8fee4adbaeb1458bca6402229
Alexander Zagniotov <azagniotov@gmail.com>
2013-12-19 18:38:44 -0800

    DSGdfgdf

--- uninstall.php
+++ uninstall.php
@@ -1,10 +1,14 @@
 <?php
 
-if(defined('WP_UNINSTALL_PLUGIN') ) {
+if(defined('WP_UNINSTALL_PLUGIN')) {
     global $wpdb;
 
     $options_table = $wpdb->options;
-    $wpdb->query( "DELETE FROM ".$options_table." WHERE option_name LIKE '".CGMP_ALL_MAP_CACHED_CONSTANTS_PREFIX."%'" );
+    $options_to_delete = $wpdb->query( "SELECT option_name FROM ".$options_table." WHERE option_name LIKE '".CGMP_ALL_MAP_CACHED_CONSTANTS_PREFIX."%'" );
+
+    foreach ( $options_to_delete as $option_name ) {
+        delete_option($option_name);
+    }
 
     //legacy
     delete_option(CGMP_DB_PUBLISHED_POST_MARKERS);
